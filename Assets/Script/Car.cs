624a3fdbf5057cdeb09982d9851eb578df1cf11c
Su Huan Li <sueunha520@gmail.com>
2022-03-17 22:20:57 -0400

    Assignment-1

---
+++ Assets/Script/Car.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 dir = Vector2.right;
+        Vector3 vel = dir * speed * Time.deltaTime;
+        transform.Translate(vel);
+    }
+}
